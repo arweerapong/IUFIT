6d74a6868354d4398ccf5671135907fbd1e490ee
arweerapong <ar.weerapong@gmail.com>
2026-06-10 21:38:10 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* เวลาอัปเดตแอป ให้เปลี่ยน v1 -> v2 -> v3 ... ผู้ใช้จะได้ตัวใหม่อัตโนมัติ */
-const CACHE='iufit-v11';
+const CACHE='iufit-v12';
 const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim()});
