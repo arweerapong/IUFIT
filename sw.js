b5d7d8ed4d39fe02de5910bca0939036abc86441
IUAPP <ar.weerapong@gmail.com>
2026-06-19 04:11:15 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* เวลาอัปเดตแอป ให้เปลี่ยนเลขเวอร์ชัน v58 -> v59 ... */
-const CACHE='iufit-v117-modern-ui';
+const CACHE='iufit-v119-modern-ui';
 const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./iufit-modern-theme.css'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim()});
