afd89de365be2a1387896f01586d0080152ab59a
Ar Weerapong <ar.weerapong@gmail.com>
2026-07-26 02:21:20 +0700

    deploy: switch web app to Vue build

---
+++ assets/CoachCalcSheet-oc1_Wfy8.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CoachCalcSheet.vue_vue_type_script_setup_true_lang-BzO8sper.js";import"./vendor-DzIf5zqS.js";import"./index-BefVIydU.js";import"./IuIcon-DJHsp-Cj.js";export{o as default};
