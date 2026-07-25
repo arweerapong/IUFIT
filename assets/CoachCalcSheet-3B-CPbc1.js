e768857d6644956856d8b537c0d6f27a558b86e5
Ar Weerapong <ar.weerapong@gmail.com>
2026-07-26 04:14:45 +0700

    deploy: switch web app to Vue build

---
+++ assets/CoachCalcSheet-3B-CPbc1.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CoachCalcSheet.vue_vue_type_script_setup_true_lang-Cu2MrYg5.js";import"./vendor-DzIf5zqS.js";import"./index-BinrbTxI.js";import"./IuIcon-CmB5zgCu.js";export{o as default};
