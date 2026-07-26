7d12f7da7ac8689f79ccd8f67a962b45ec58dc86
Ar Weerapong <ar.weerapong@gmail.com>
2026-07-27 02:47:00 +0700

    deploy: switch web app to Vue build

---
+++ assets/CoachCalcSheet-BX2n9Y9Z.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CoachCalcSheet.vue_vue_type_script_setup_true_lang-ClqgYac_.js";import"./vendor-BVC6_PxF.js";import"./index-C3UA2_PS.js";import"./IuIcon-frXBJ7IB.js";export{o as default};
---
+++ assets/IuMateView-DYYfbK_U.js
@@ -0,0 +1,1 @@
+import e from"./IuMateSheet-Kju58RoZ.js";import{e as t,m as a,y as o}from"./vendor-BVC6_PxF.js";import"./index-C3UA2_PS.js";import"./IuIcon-frXBJ7IB.js";const _=t({__name:"IuMateView",setup(r){return(p,m)=>(o(),a(e,{variant:"page"}))}});export{_ as default};
