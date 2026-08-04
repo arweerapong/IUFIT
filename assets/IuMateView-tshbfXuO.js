e24b764e7369c37ac0f53d9dc2a92a22d531b48e
Ar Weerapong <ar.weerapong@gmail.com>
2026-08-05 00:04:18 +0700

    deploy: switch web app to Vue build

---
+++ assets/IuMateView-tshbfXuO.js
@@ -0,0 +1,1 @@
+import e from"./IuMateSheet-DYYzRWeg.js";import{e as t,m as a,y as o}from"./vendor-O35ZN0d3.js";import"./index-XEdKuUh0.js";import"./IuIcon-F4Gzs5x2.js";const _=t({__name:"IuMateView",setup(r){return(p,m)=>(o(),a(e,{variant:"page"}))}});export{_ as default};
---
+++ assets/WeightView-CMsmEvx1.js
@@ -0,0 +1,1 @@
+import{e as t,f as o,g as r,x as a,y as n}from"./vendor-O35ZN0d3.js";import{c as s}from"./ResDrillOpen-CB3FH2_X.js";import"./index-XEdKuUh0.js";const c={"aria-hidden":"true"},f=t({__name:"WeightView",setup(i){const e=a();return o(async()=>{await e.replace("/body"),s("w")}),(p,_)=>(n(),r("section",c))}});export{f as default};
