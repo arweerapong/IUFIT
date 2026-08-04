46cae5d527e2160f684423066aca9bf1eee311b9
Ar Weerapong <ar.weerapong@gmail.com>
2026-08-05 03:07:59 +0700

    deploy: switch web app to Vue build

---
+++ assets/IuMateView-DwJMV9p6.js
@@ -0,0 +1,1 @@
+import e from"./IuMateSheet-BzLFck0j.js";import{e as t,m as a,y as o}from"./vendor-Bw2caL45.js";import"./index-Dwvy_azU.js";import"./IuIcon-DaXYGQb1.js";const _=t({__name:"IuMateView",setup(r){return(p,m)=>(o(),a(e,{variant:"page"}))}});export{_ as default};
