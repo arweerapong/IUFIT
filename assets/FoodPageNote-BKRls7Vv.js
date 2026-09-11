556e223354d4b611af3f50550f498ded39dd5e47
Ar Weerapong <ar.weerapong@gmail.com>
2026-09-11 09:10:48 +0700

    deploy: switch web app to Vue build

---
+++ assets/FoodPageNote-BKRls7Vv.js
@@ -0,0 +1,1 @@
+import{a as t,l as e,t as a,o as s}from"./vendor-D8Wzbpvg.js";import{Q as c}from"./index-CJuJzuXb.js";const n={class:"fpn","data-block":"note"},p=t({__name:"FoodPageNote",props:{text:{}},setup(o){return(r,_)=>(s(),e("p",n,a(o.text),1))}}),d=c(p,[["__scopeId","data-v-ffc3b0e4"]]);export{d as F};
